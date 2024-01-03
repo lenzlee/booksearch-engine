8f81c96ad7da902af46e0114150d696038a6bd9f
Lenzlee Wheeler <lwheeler@luc.edu>
2024-01-02 21:43:53 -0600

    updated all files

---
+++ README.md
@@ -0,0 +1,30 @@
+# MERN Challenge: Book Search Engine
+
+In this challenge, we were asked to take a fully functioning Google Books API search engine built with a RESTful API, and refactor it to be a GraphQL API built with Apollo Server. The app was built using the MERN stack, with a React front end, MongoDB database, and Node.js/Express.js server and API. It's already set up to allow users to save book searches to the back end.
+
+In this Challenge, we were asked to set up an Apollo Server to use GraphQL queries and mutations to fetch and modify data, replacing the existing RESTful API. We were also asked to modify the existing authentication middleware so that it works in the context of a GraphQL API. Lastly, we were asked to create an Apollo Provider so that requests can communicate with an Apollo Server.
+
+This application was deployed to Render.
+
+## URLs Submitted:
+
+* URL of the functional, deployed application on Render: https://booksearch-engine-yuwi.onrender.com
+
+* GitHub Repo URL: https://github.com/lenzlee/booksearch-engine
+
+## References and Resources: 
+I mostly used our Module 21 lessons for this assignment. I also referred to the following resources:
+
+Apollo Client: https://www.apollographql.com/docs/react/
+
+GraphQL: https://graphql.com/learn/what-is-graphql/
+
+Render: https://docs.render.com/deploys
+
+express.js: https://www.npmjs.com/package/express
+
+Mongoose: https://www.npmjs.com/package/mongoose
+
+
+## Contact Me
+Lenzlee Wheeler at: https://github.com/lenzlee 
--- server/server.js
+++ server/server.js
@@ -1,21 +1,36 @@
 const express = require('express');
-const path = require('path');
+// Import the ApolloServer class and expressMiddleware helper function
+const { ApolloServer } = require('@apollo/server');
+const { expressMiddleware } = require('@apollo/server/express4');
+
+// Import the two parts of a GraphQL schema
+const { typeDefs, resolvers } = require('./schemas');
 const db = require('./config/connection');
-const routes = require('./routes');
+
+const PORT = process.env.PORT || 3001;
+const server = new ApolloServer({
+  typeDefs,
+  resolvers
+});
 
 const app = express();
-const PORT = process.env.PORT || 3001;
 
-app.use(express.urlencoded({ extended: true }));
-app.use(express.json());
+// Create a new instance of an Apollo server with the GraphQL schema
+const startApolloServer = async () => {
+  await server.start();
+  
+  app.use(express.urlencoded({ extended: false }));
+  app.use(express.json());
 
-// if we're in production, serve client/build as static assets
-if (process.env.NODE_ENV === 'production') {
-  app.use(express.static(path.join(__dirname, '../client/build')));
-}
+  app.use('/graphql', expressMiddleware(server));
 
-app.use(routes);
+  db.once('open', () => {
+    app.listen(PORT, () => {
+      console.log(`API server running on port ${PORT}!`);
+      console.log(`Use GraphQL at http://localhost:${PORT}/graphql`);
+    })
+  })
+};
 
-db.once('open', () => {
-  app.listen(PORT, () => console.log(`🌍 Now listening on localhost:${PORT}`));
-});
+// Call the async function to start the server
+startApolloServer();
